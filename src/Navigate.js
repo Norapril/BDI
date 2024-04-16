0e956b3c12440aceac12c6cb7cb5ba850800f422
Norapril <norapril428@gmail.com>
2024-04-16 10:34:22 -0700

    Update Navigate.js

--- src/Navigate.js
+++ src/Navigate.js
@@ -1,8 +1,12 @@
 import { useViewport } from './viewportContext';
 import MobileComponent from './MobileComponent';
 import DesktopComponent from './DesktopComponent';
+import React, { useEffect } from 'react';
 
 const Navigate = () => {
+	useEffect(() => {
+		document.title = "BDI测试";
+	  }, []);
 	const { width } = useViewport();
 	const breakpoint = 760;
 	return width < breakpoint ? <MobileComponent /> : <DesktopComponent />;
